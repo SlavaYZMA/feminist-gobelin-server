3c24365e4c3a2b2d7553c7ada59546a858f6374a
SIava <slavasolen@gmail.com>
2025-07-31 22:06:21 +0200

    Исправил порт в server.py для Render

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -37,4 +38,5 @@ def generate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
